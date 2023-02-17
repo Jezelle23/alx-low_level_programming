2e10d62de350eff1f7051fa877d994e00406bf9d
jezereal <jezerealgilbert@gmail.com>
2023-02-17 09:05:41 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Print out digits
+ * Description: Print out all single digits of base ten starting from zero
+ * using putchar, followed by a new line
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+        int digit;
+
+        for (digit = 0; digit < 10; digit++)
+        {
+                putchar((digit % 10) + '0');
+        }
+        putchar('\n');
+        return (0);
+}
