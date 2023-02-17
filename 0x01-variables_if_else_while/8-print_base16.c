6343616311e47948c7629dc1a433f82c5a83605e
jezereal <jezerealgilbert@gmail.com>
2023-02-17 09:55:28 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Print out base numbers
+ * Description: All base numbers of sixteen in lower case
+ * Return: Always0 (Success)
+ */
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar((number % 10) + '0');
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
