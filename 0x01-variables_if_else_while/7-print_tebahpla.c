9c82fd5bcb54344bc330b6328f9f2bb5c22ca92f
jezereal <jezerealgilbert@gmail.com>
2023-02-17 09:27:12 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Print the letters of the aplhabet
+ * Description: Letters of the alphabet displayed in lower case
+ * and in reverse order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for(letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
