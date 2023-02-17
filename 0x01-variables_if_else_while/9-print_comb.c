7cf8760362390d5ff985b8f9ded7cc2011615042
jezereal <jezerealgilbert@gmail.com>
2023-02-17 10:28:01 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Print all combination of single digit numbers
+ * Description: Numbers should only be printed in ascending order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
