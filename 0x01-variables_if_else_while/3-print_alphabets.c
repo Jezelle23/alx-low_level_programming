621f45b3cb20d6745cfe7f519ee34871e886bf94
jezereal <jezerealgilbert@gmail.com>
2023-02-17 06:43:39 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Print the alphabets
+ * Description: Display in lowers case and in Upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter ++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter ++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
