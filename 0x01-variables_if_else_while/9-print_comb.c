18a2d4ac7dba2f5e445d96ed90f0d045d498814d
jezereal <jezerealgilbert@gmail.com>
2023-02-17 14:33:08 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,10 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
-		if (num < 9){
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
