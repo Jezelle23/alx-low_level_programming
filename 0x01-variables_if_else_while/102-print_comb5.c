42883edc63fd8d66a437958db45e7294643a0272
jezereal <jezerealgilbert@gmail.com>
2023-02-19 08:27:37 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,10 +6,7 @@
  */
 int main(void)
 {
-	int dig1;
-	int dig2;
-	int dig3;
-	int dig4;
+	int dig1, dig2, dig3, dig4;
 
 	for (dig1 = 0; dig1 < 10; dig1++)
 	{
@@ -23,10 +20,11 @@ int main(void)
 					{
 						continue;
 					}
-					if (dig1 % 10 == 1 && dig2 % 10 ==1 && dig3 % 10 ==1 && dig4 % 10 ==1)
+					if (dig1 == dig3 && dig2 == dig4)
 					{
 						continue;
 					}
+
 					putchar(dig1 + '0');
 					putchar(dig2 + '0');
 
@@ -35,10 +33,6 @@ int main(void)
 					putchar(dig3 + '0');
 					putchar(dig4 + '0');
 
-					if (dig1 == 9 && dig2 == 9 && dig3 == 9 && dig4 == 9)
-					{
-						continue;
-					}
 					putchar(',');
 					putchar(' ');
 				}
