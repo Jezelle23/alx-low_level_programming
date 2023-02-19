edddf2633e8c7b5b346eb22cf770d4907ed700ba
jezereal <jezerealgilbert@gmail.com>
2023-02-19 06:00:38 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,33 +6,41 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+	int digit1;
+	int digit2;
+	int digit3;
+	int digit4;
 
 	for (digit1 = 0; digit1 < 10; digit1++)
 	{
 		for (digit2 = 0; digit2 < 10; digit2++)
 		{
-			if (digit1 == 0 && digit2 == 0)
+			for (digit3 = 0; digit3 < 10; digit3++)
 			{
-				continue;
+				for (digit4 = 0; digit4 < 10; digit4++)
+				{
+					if (digit1 == 0 && digit2 == 0 && digit3 == 0 && digit4 == 0)
+					{
+						continue;
+					}
+					putchar(digit1 + '0');
+					putchar(digit2 + '0');
+
+
+					putchar(' ');
+
+					putchar(digit3 + '0');
+					putchar(digit4 + '0');
+
+					if (digit1 == 9 && digit2 == 9 && digit3 == 9 && digit4 ==9)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			putchar(0 + '0');
-			putchar(0 + '0');
-
-
-			putchar(' ');
-
-			putchar(digit1 + '0');
-			putchar(digit2 + '0');
-
-			if (digit1 == 9 && digit2 == 9)
-			{
-				continue;
-			}
-			putchar(',');
-			putchar(' ');
 		}
-		putchar('\n');
 	}
 	return (0);
 }
