9be70be8a34d0dc29bb4ad8b9af99b32b1b0feb8
jezereal <jezerealgilbert@gmail.com>
2023-02-23 06:10:40 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - checks for checks for a digit (0 through 9)
+ * @a: variable
+ * @b: variable
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
