a18ccfb7c8b29316f3970c34014d761e7bc95429
jezereal <jezerealgilbert@gmail.com>
2023-03-01 11:56:02 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,20 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		x = -x;
 	}
-	if ((x / 10) > 0)
-		print_number(x / 10);
+	else
+	{
+		n1 = n;
+	}
 
-	_putchar((x % 10) + '0');
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
