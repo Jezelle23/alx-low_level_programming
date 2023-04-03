cadbf4393a07dd64890dacae563a97dbdc10de96
jezereal <jezerealgilbert@gmail.com>
2023-04-03 11:39:10 -0700

    advanced tasks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,31 +2,38 @@
 
 /**
  * free_listint_safe - frees a list
- * @h: the pointer to the start of list to free
- * Return: (Countn)
+ * @h: pointer to the start of list to free
+ * Return: (var1) Success
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t Countn = 0;
-	listint_t *temp = NULL;
+	size_t var1 = 0;
+	int num;
+	listint_t *temp;
 
-	if (!(h && *h))
-		return (Countn);
+	if (!h || !*h)
+		return (0);
+
 	while (*h)
 	{
-		Countn++;
-		if (*h > (*h)->next)
+		num = *h - (*h)->next;
+		if (num > 0)
 		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			var1++;
 		}
 		else
 		{
 			free(*h);
 			*h = NULL;
+			var1++;
+			break;
 		}
 	}
+
 	*h = NULL;
-	return (Countn);
+
+	return (var1);
 }
