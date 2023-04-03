bc30289b5c460a00ced96d47a62ff59e8ea1c5d7
jezereal <jezerealgilbert@gmail.com>
2023-04-03 11:04:40 -0700

    lists.h updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,6 +12,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+static listint_t *new_node(const int n);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
