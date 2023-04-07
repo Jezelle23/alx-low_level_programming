8cfba93168b8175e55c73833521ee1ffe8066758
jezereal <jezerealgilbert@gmail.com>
2023-04-07 09:14:55 -0700

    quest 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,29 +11,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, res, len = 0;
+	int num;
+	int write_x; 
+	int len = 0;
 
-	if (!filename)
-	{
+	if (filename == NULL)
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-	{
+	num = open(filename, O_WRONLY | O_APPEND);
+	write_x = write(o, text_content, len);
+
+	if (num == -1 || write_x == -1)
 		return (-1);
-	}
-	if (text_content)
-	{
-		len = strlen(text_content);
-		res = write(fd, text_content, len);
-		if (res == -1 || res != len)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
 
-	close(fd);
+	close(num);
+
 	return (1);
 }
